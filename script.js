f2433ae7cae20afad1a5d077c31508d66862a40e
marioCarvalho001 <mariocarvalho755@gmail.com>
2021-06-04 19:22:54 -0300

    primeiro commit no vscode

---
+++ script.js
@@ -0,0 +1,5 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click",function(){
+    var sitebar = document.querySelector(".container").classList.toggle("show-menu")
+});
